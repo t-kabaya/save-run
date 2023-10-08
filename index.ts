554d23f33d042303538d5197f426f1c3c73dfa66
Kabaya Taiki <taikikabaya@gmail.com>
2023-10-08 21:34:22 +0900

    エラーが発生してもリスタートするよう修正

--- index.ts
+++ index.ts
@@ -6,9 +6,32 @@ import { throttle } from 'lodash'
 
 const argument = process.argv.slice(2).join(' ')
 
-const _run = () => execSync(argument, { stdio: 'inherit' })
+// Initialize watcher.
+const watcher = watch('.', {
+  persistent: true
+});
+
+const _run = (arg: string) => {
+  if (!arg) {
+    console.log('Please provide a path as an argument.')
+    console.log('eg: npx save-run yarn run test')
+
+    watcher.close()
+    return
+  }
+
+  try {
+    execSync(arg, { stdio: 'inherit' })
+  } catch (e) {
+    console.log('ERROR: restarting...')
+    _run(arg)
+  }
+} 
+
 const run = throttle(_run, 1000)
 
 // run script
-run()
-watch('.').on('change', run)
+watcher.on('change', () => run(argument))
+// .onはファイルを保存しないと実行されないので、初回実行のためにrunを実行
+run(argument)
+
