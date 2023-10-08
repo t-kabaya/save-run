47edfa606de185f40b5bd2520576b42220fa9b88
Kabaya Taiki <taikikabaya@gmail.com>
2023-10-08 21:24:39 +0900

    ts-nodeのエラーを消すためにoption変数自体を削除した。

--- index.ts
+++ index.ts
@@ -5,10 +5,8 @@ import  { execSync } from 'child_process'
 import { throttle } from 'lodash'
 
 const argument = process.argv.slice(2).join(' ')
-const option = { stdio: 'inherit' }
 
-// @ts-ignore
-const _run = () => execSync(argument, option)
+const _run = () => execSync(argument, { stdio: 'inherit' })
 const run = throttle(_run, 1000)
 
 // run script
